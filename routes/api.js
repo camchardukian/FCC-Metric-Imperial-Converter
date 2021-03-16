4d6b9a1e6d96e3a1673f4a5960517ec01e341f23
Cameron Chardukian <camchardukian@gmail.com>
2021-03-16 18:03:50 +0700

    updated the README file and added a convert route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
   let convertHandler = new ConvertHandler();
-
+  app.route("/api/convert").get((req, res, next) => {
+    console.log("i rannnnn", req);
+    let input = req.query.input;
+    console.log("input", input);
+  });
 };

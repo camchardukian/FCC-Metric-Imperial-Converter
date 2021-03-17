0e8459124428a467eef90250ce450e61c789ffc6
Cameron Chardukian <camchardukian@gmail.com>
2021-03-17 18:13:33 +0700

    feature/hard-code-return-JSON-string

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,15 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function(app) {
   let convertHandler = new ConvertHandler();
+
   app.route("/api/convert").get((req, res, next) => {
-    console.log("i rannnnn", req);
     let input = req.query.input;
-    console.log("input", input);
+    res.json({
+      initNum: 3.1,
+      initUnit: "mi",
+      returnNum: 4.98895,
+      returnUnit: "km",
+      string: "3.1 miles converts to 4.98895 kilometers"
+    });
   });
 };
